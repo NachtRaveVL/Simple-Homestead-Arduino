828d81a8c7d97479d6d57685ed21189e8fbc3ccd
NachtRave <NachtRaveVL@users.noreply.github.com>
2026-08-22 13:31:44 -0600

    fixing data

--- src/TerraData.h
+++ src/TerraData.h
@@ -1,30 +1,97 @@
 /*  Terraduino: Simple automation controller for homestead resource and environmental systems.
     Copyright (C) 2026 NachtRaveVL
-    Terraduino System Data
+    Terraduino Data
 */
 
 #ifndef TerraData_H
 #define TerraData_H
 
-#include "TerraSetup.h"
-#include "TerraInterfaces.h"
+struct TerraData;
+struct TerraSubData;
 
-TerraString terraJsonEscape(const TerraString &value);
-bool terraJsonExtractString(const TerraString &json, const char *key, TerraString &out);
-bool terraJsonExtractLong(const TerraString &json, const char *key, long &out);
-bool terraJsonExtractFloat(const TerraString &json, const char *key, float &out);
-bool terraJsonExtractBool(const TerraString &json, const char *key, bool &out);
+#include "Terraduino.h"
 
-struct TerraSystemData : public TerraSerializable {
-    TerraSystemSetup setup;                                 // Saved setup data
-    uint32_t sequence;                                      // Saved system-data sequence
+// Serializes terraduino data structure to a binary output stream (essentially a memcpy), with optional skipBytes
+extern size_t serializeDataToBinaryStream(const TerraData *data, Stream *streamOut, size_t skipBytes = sizeof(void*));
+// Deserializes terraduino data structure from a binary input stream (essentially a memcpy), with optional skipBytes
+extern size_t deserializeDataFromBinaryStream(TerraData *data, Stream *streamIn, size_t skipBytes = sizeof(void*));
 
-    TerraSystemData() : setup(), sequence(0) { }
-    TerraString toJSON() const override;
-    bool fromJSON(const TerraString &json);
-    size_t binarySize() const;
-    size_t toBinary(uint8_t *buffer, size_t capacity) const;
-    bool fromBinary(const uint8_t *buffer, size_t length);
+// Creates a new terraduino data object corresponding to a binary input stream (return ownership transfer - user code *must* delete returned data)
+extern TerraData *newDataFromBinaryStream(Stream *streamIn);
+// Creates a new terraduino data object corresponding to an input JSON element (return ownership transfer - user code *must* delete returned data)
+extern TerraData *newDataFromJSONObject(JsonObjectConst &objectIn);
+
+
+// Data Base
+// Base class for serializable (JSON+Binary) storage data, used to define the base
+// header of all data stored internally.
+// NOTE: NON-CONST VALUE TYPES ONLY. All data *MUST* be able to use default operator=, constructor, and destructor.
+struct TerraData : public TerraJSONSerializableInterface {
+    union {
+        char chars[4];                                      // Standalone data structure 4-char identifier
+        struct {
+          tid_t idType;                                     // Object ID type enum value (e.g. actuator, sensor, etc.)
+          tid_t objType;                                    // Object type enum value (e.g. actuatorType, sensorType, etc.)
+          tposi_t posIndex;                                 // Object position index # (zero-ordinal)
+          tid_t classType;                                  // Object class type enum value (e.g. pump, dht1w, etc.)
+        } object;
+    } id;                                                   // Identifier union
+    uint16_t _size;                                         // The size (in bytes) of the data
+    uint8_t _version;                                       // Version # of data container
+    int8_t _revision;                                       // Revision # of stored data (uses -vals for modified flag)
+
+    inline bool isStandardData() const { return id.chars[0] == 'H'; }
+    inline bool isSystemData() const { return isStandardData() && id.chars[1] == 'S' && id.chars[2] == 'Y' && id.chars[3] == 'S'; }
+    inline bool isCalibrationData() const { return isStandardData() && id.chars[1] == 'C' && id.chars[2] == 'A' && id.chars[3] == 'L'; }
+    inline bool isCropsLibData() const { return isStandardData() && id.chars[1] == 'C' && id.chars[2] == 'L' && id.chars[3] == 'D'; }
+    inline bool isAdditiveData() const { return isStandardData() && id.chars[1] == 'A' && id.chars[2] == 'D' && id.chars[3] == 'D'; }
+    inline bool isUIData() const { return isStandardData() && id.chars[1] == 'U' && id.chars[2] == 'I' && id.chars[3] == 'D'; }
+    inline bool isObjectData() const { return !isStandardData() && id.object.idType >= 0; }
+
+    TerraData();                                            // Default constructor
+    TerraData(char id0,                                     // 4-char identifier, index 0
+              char id1,                                     // 4-char identifier, index 1
+              char id2,                                     // 4-char identifier, index 2
+              char id3,                                     // 4-char identifier, index 3
+              uint8_t version = 1,                          // Data structure version #
+              uint8_t revision = 1);                        // Stored data revision #
+    TerraData(tid_t idType,                                 // ID type enum value
+              tid_t objType,                                // Object type enum value
+              tposi_t posIndex,                             // Object position index #
+              tid_t classType,                              // Class type enum value
+              uint8_t version = 1,                          // Data structure version #
+              uint8_t revision = 1);                        // Stored data revision #
+    TerraData(const TerraIdentity &id);                     // Identity constructor
+
+    virtual void toJSONObject(JsonObject &objectOut) const override;
+    virtual void fromJSONObject(JsonObjectConst &objectIn) override;
+    virtual void migrateFromBinaryVersion(uint8_t) { ; }
+
+    inline uint8_t getRevision() const { return abs(_revision); }
+    inline bool isModified() const { return _revision < 0; }
+    inline void bumpRevisionIfNeeded() { if (!isModified()) { _revision = -(abs(_revision) + 1); } } // Should be called before modifying data
+    inline void unsetModified() { _revision = abs(_revision); } // Should be called after save-out
 };
+
+
+// Sub Data Base
+// Sub-data exists inside of regular data for smaller objects that don't require the
+// entire data object hierarchy, useful for triggers, measurements, etc.
+// NOTE: NON-CONST VALUE TYPES ONLY, NO VIRTUALS. All data *MUST* be able to use default operator=.
+struct TerraSubData {
+    tid_t type;                                             // Sub data type, else -1/none
+
+    TerraSubData();
+    TerraSubData(tid_t dataType);
+    void toJSONObject(JsonObject &objectOut) const;
+    void fromJSONObject(JsonObjectConst &objectIn);
+
+    inline bool isSet() const { return isValidType(type); }
+};
+
+
+// Internal use, but must contain all ways for all data types to be new'ed
+extern TerraData *_allocateDataFromBaseDecode(const TerraData &baseDecode);
+extern TerraData *_allocateDataForObjType(int8_t idType, int8_t classType);
 
 #endif
