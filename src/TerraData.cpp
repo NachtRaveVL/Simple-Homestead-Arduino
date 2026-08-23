d567c436371295aedf5ab0829d5fcc19bae0b5dc
NachtRave <NachtRaveVL@users.noreply.github.com>
2026-08-22 18:13:34 -0600

    Update TerraData.cpp

--- src/TerraData.cpp
+++ src/TerraData.cpp
@@ -124,7 +124,7 @@ void TerraData::fromJSONObject(JsonObjectConst &objectIn)
 {
     JsonVariantConst idVar = objectIn[SFP(TStr_Key_Type)];
     const char *idStr = idVar.as<const char *>();
-    if (idStr && idStr[0] == 'H') {
+    if (idStr && idStr[0] == 'T') {
         strncpy(id.chars, idStr, 4);
     } else if (idStr) {
         int8_t typeVals[4];
